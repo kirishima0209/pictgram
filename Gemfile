94450f4906e9813eec63cd8283b56f545d8b0b16
mzze <sorasorasora.103@gmail.com>
2019-08-30 16:28:23 +0000

    fourth commit

--- Gemfile
+++ Gemfile
@@ -73,3 +73,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'jquery-rails'
 
 gem 'bootstrap-sass'
+
+gem 'bcrypt'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 15 } 
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :password, length: { minimum: 8, maximum: 32 }, format: { with: /\A(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]+\z/i }
+
+   has_secure_password
 end
